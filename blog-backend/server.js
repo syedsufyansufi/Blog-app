d82499a5e35157857eacc74238a742313c6cb9fc
syedsufyansufi <syedsufiyan787@gmail.com>
2025-08-16 16:15:27 +0530

    port changes

--- blog-backend/server.js
+++ blog-backend/server.js
@@ -96,7 +96,7 @@ app.delete("/posts/:id", async (req, res) => {
 });
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Visit: http://localhost:${PORT}`);
